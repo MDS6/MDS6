c911c17c60d52b102fdf15eb6367823997725059
Diaztri Hazam <111562026+diaztrihazam@users.noreply.github.com>
2023-03-07 10:36:36 +0700

    add data sql

---
+++ data/yo_linkshare_dummy.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: select yo_linkshare.id,yo_linkshare.name,yo_linkshare.poin,yo_linkshare.created_date,yo_linkshare.modified_date 
+	from yo_linkshare
+-- Date: 2023-02-26 18:06
+*/
+INSERT INTO yo_linkshare
+VALUES  (293,'P_Cone2022_smspre',0,'2022-11-03 05:04:25',NULL),
+		(294,'P_Cone2022',0,'2022-11-03 05:05:48','2022-11-03 05:06:20'),
+		(295,'P_Cone2022_smspost',0,'2022-11-03 05:07:16',NULL),
+		(296,'P_Choice',NULL,'2022-11-24 10:40:05',NULL),
+		(297,'P_Pyramid',NULL,'2022-12-01 12:22:58',NULL),
+		(298,'P_Vertex',NULL,'2022-12-01 12:24:35',NULL),
+		(299,'P_Vertex',NULL,'2022-12-01 12:24:47',NULL),
+		(300,'P_Short',NULL,'2022-12-01 12:25:01',NULL),
+		(301,'P_Short',NULL,'2022-12-01 12:25:15',NULL),
+		(302,'P_Octagon',NULL,'2022-12-09 13:38:34',NULL);
